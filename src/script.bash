bc38d35023ac7c4099d7e9e0ddd92c4888b600c4
Manasi1999 <manasigude@gmail.com>
2021-10-20 19:07:12 +0530

    Regression Done

--- src/script.bash
+++ src/script.bash
@@ -1,14 +1,14 @@
-i = 0
-for S in 100 1000 1000
+i=0
+for S in 10 100
 do
-    for F in 10 20 50 
+    for F in 10
     do 
         for M in 1 0
         do 
             for V in 1 0.1 0.01 
             do 
-                papermill Dataset1-Regression.ipynb RegressionOutput/Dataset1-Regression_output_${i}.ipynb -p n_samples ${S} -p n_features ${F} -p mean ${M} -p variance ${V} -k papermill-tutorial
-                jupyter nbconvert RegressionOutput/Dataset1-Regression_output_${i}.ipynb --to pdf
+                papermill Dataset1-Regression.ipynb Dataset1-Regression_output_${i}.ipynb -p n_samples ${S} -p n_features ${F} -p mean ${M} -p variance ${V} -k papermill-tutorial
+                jupyter nbconvert Dataset1-Regression_output_${i}.ipynb --to pdf
                 ((i=i+1))
             done 
         done 
