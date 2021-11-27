b38f27b16a6bdc720931f82c536367cf86b4a9b7
Sharvari Hedaoo <hedaoosharvari1972@gmail.com>
2021-11-27 12:16:18 +0530

    script.bash to script.sh, removed nbconvert for now

---
+++ Notebooks/Boston_Dataset/script.sh
@@ -0,0 +1,29 @@
+# Run Baseline Models  
+
+# for runs in 1 2 3 4 5 6 7 8 9 10
+# do 
+#     papermill BaselineModels.ipynb ./BaseLine_Model_Output/baselineModels_output_${runs}.ipynb  -k papermill-tutorial
+
+# done 
+
+
+#Run GAN Model 
+#for runs in 1 2 3 4 5 6 7 8 9 10
+#do 
+#    papermill GAN.ipynb ./Main/GAN_output_${runs}.ipynb  -k papermill-tutorial
+
+#done 
+
+#Run ABC-GAN Model
+ i=0 
+ for runs in 1 2 3 4 5 6 7 8 9 10
+ do
+     for M in 1 0
+     do 
+         for V in 1 0.1 0.01 
+         do 
+             papermill ABC_GAN.ipynb ./Main/ABC-GAN_output_${i}.ipynb -p mean ${M} -p variance ${V} -p n_epochs 5000 -k papermill-tut
+             ((i=i+1))
+         done 
+     done
+ done 
