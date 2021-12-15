3278b012b90e7acfc62145c790ff578e4a82b7f8
Sharvari Hedaoo <hedaoosharvari1972@gmail.com>
2021-12-15 18:05:31 +0530

    Modified network TAB net like + GAN results

--- Notebooks/Boston_Dataset/script.sh
+++ Notebooks/Boston_Dataset/script.sh
@@ -8,25 +8,25 @@
 
 
 #Run GAN Model 
-#for runs in 1 2 3 4 5 6 7 8 9 10
-#do 
-#    papermill GAN.ipynb ./Main/GAN_output_${runs}.ipynb  -k papermill-tutorial
+for runs in 1 2 3 4 5 6 7 8 9 10
+do 
+    papermill GAN.ipynb ./Main/GAN_output_${runs}.ipynb  -k papermill-tut
 
-#done 
+done 
 
 #Run ABC-GAN Model
-# for runs in 2 3
-# do
-#     i=0
-#     for M in 1 0
-#     do 
-#         for V in 1 0.1 0.01 
-#         do 
-#             papermill ABC_GAN.ipynb ./Main/ABC-GAN_output_${i}_${runs}.ipynb -p abc_mean ${M} -p variance ${V} -p n_epochs 5000 -k papermill-tutorial
-#             ((i=i+1))
-#         done 
-#     done
-# done 
+for runs in 2 3
+do
+     i=0
+     for M in 1 0
+     do 
+         for V in 1 0.1 0.01 
+         do 
+             papermill ABC_GAN.ipynb ./Main/ABC-GAN_output_${i}_${runs}.ipynb -p abc_mean ${M} -p variance ${V} -p n_epochs 5000 -k papermill-tut             
+             ((i=i+1))
+         done 
+     done
+done 
 
 
 #Analysis 
--- src/network.py
+++ src/network.py
@@ -24,14 +24,17 @@ class Discriminator(nn.Module):
 class Generator(nn.Module):
   def __init__(self,n_input):
     super().__init__()
-    self.hidden1 = nn.Linear(n_input,100)
-    self.hidden2 = nn.Linear(100,100)
-    self.output = nn.Linear(100,1)
+    self.hidden1 = nn.Linear(n_input,50)
+    self.hidden2 = nn.Linear(50,50)
+    self.output = nn.Linear(50,1)
     self.relu = nn.ReLU()
 
   def forward(self, x):
     x = self.hidden1(x)
     x = self.relu(x)
+    x = self.hidden2(x)
+    x = self.hidden2(x)
+    x = self.hidden2(x)
     x = self.hidden2(x)
     x = self.relu(x)
     x = self.output(x)
