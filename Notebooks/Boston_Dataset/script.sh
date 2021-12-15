5f3f81f88d0ac55b04f2dc24e501d15c29b3935d
Manasi1999 <manasigude@gmail.com>
2021-12-15 15:50:16 +0530

    Script updated

--- Notebooks/Boston_Dataset/script.sh
+++ Notebooks/Boston_Dataset/script.sh
@@ -1,18 +1,17 @@
 # Run Baseline Models  
 
-# for runs in 1 2 3 4 5 6 7 8 9 10
-# do 
-#     papermill BaselineModels.ipynb ./BaseLine_Model_Output/baselineModels_output_${runs}.ipynb  -k papermill-tutorial
+for runs in 1 2 3 4 5 6 7 8 9 10
+do 
+    papermill BaselineModels.ipynb ./BaseLine_Model_Output/baselineModels_output_${runs}.ipynb 
 
-# done 
-
+done 
 
 #Run GAN Model 
-#for runs in 1 2 3 4 5 6 7 8 9 10
-#do 
-#    papermill GAN.ipynb ./Main/GAN_output_${runs}.ipynb  -k papermill-tutorial
+for runs in 1 2 3 4 5 6 7 8 9 10
+do 
+   papermill GAN.ipynb ./Main/GAN_output_${runs}.ipynb 
 
-#done 
+done 
 
 #Run ABC-GAN Model
 # for runs in 2 3
@@ -22,15 +21,14 @@
 #     do 
 #         for V in 1 0.1 0.01 
 #         do 
-#             papermill ABC_GAN.ipynb ./Main/ABC-GAN_output_${i}_${runs}.ipynb -p abc_mean ${M} -p variance ${V} -p n_epochs 5000 -k papermill-tutorial
+#             papermill ABC_GAN.ipynb ./Main/ABC-GAN_output_${i}_${runs}.ipynb -p abc_mean ${M} -p variance ${V} -p n_epochs 5000 
 #             ((i=i+1))
 #         done 
 #     done
 # done 
 
 
-#Analysis 
-
-papermill Analysis.ipynb Analysis_Out.ipynb -k papermill-tutorial
-jupyter nbconvert Analysis_Out.ipynb --to pdf
+# #Analysis 
+# papermill Analysis.ipynb Analysis_Out.ipynb -k papermill-tutorial
+# jupyter nbconvert Analysis_Out.ipynb --to pdf
 
