1aa7e071a3f3d1029d3378be0103ae852d944e6e
Sharvari Hedaoo <hedaoosharvari1972@gmail.com>
2021-12-15 18:10:48 +0530

    TabNet + GAN outputs

--- Notebooks/Boston_Dataset/script.sh
+++ Notebooks/Boston_Dataset/script.sh
@@ -7,7 +7,6 @@
 # done 
 
 #Run GAN Model 
-<<<<<<< HEAD
 for runs in 1 2 3 4 5 6 7 8 9 10
 do 
     papermill GAN.ipynb ./Main/GAN_output_${runs}.ipynb  -k papermill-tut
@@ -27,13 +26,6 @@ do
          done 
      done
 done 
-=======
-# for runs in 1 2 3 4 5 6 7 8 9 10
-# do 
-#    papermill GAN.ipynb ./Main/GAN_output_${runs}.ipynb 
-
-# done 
->>>>>>> 4ba47d993068e5c035a1a5a01e64b2d0692cba81
 
 #Run ABC-GAN Model
 for runs in 1 2 3 4 5 6 7 8 9 10
