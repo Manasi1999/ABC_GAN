2f7f2fc766f2aded63a23e0cccd9aeed987bf86e
Manasi1999 <manasigude@gmail.com>
2021-12-17 18:00:41 +0530

    script updated

--- Notebooks/Boston_Dataset/script.sh
+++ Notebooks/Boston_Dataset/script.sh
@@ -28,14 +28,14 @@
 # done 
 
 #Run ABC-GAN Model
-for runs in 1
+for runs in 1 2 3 4 5 6 7 8 9 10
 do
     i=0
     for M in 1 0
     do 
         for V in 1 0.1 0.01 
         do 
-            papermill ABC_GAN-Skip_Connection.ipynb ./Main/ABC-GAN_output_${i}_${runs}.ipynb -p abc_mean ${M} -p variance ${V} -p n_epochs 5000 -k papermill-tutorial
+            papermill ABC_GAN.ipynb ./Main/ABC-GAN_output_${i}_${runs}.ipynb -p abc_mean ${M} -p variance ${V} -p n_epochs 5000 -k papermill-tut
             ((i=i+1))
         done 
     done
