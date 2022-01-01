d0891c67f3e6a487d4742b6ef04af1b084474bce
Manasi1999 <manasigude@gmail.com>
2022-01-01 18:37:51 +0530

    script file added

---
+++ Notebooks/California_Dataset/script2.sh
@@ -0,0 +1,9 @@
+for runs in 1 2 3 4 5 6 7 8 9 10
+do
+    i=0
+    for V in 1 0.1 0.01 
+    do 
+        papermill ABC_GAN_Model-Catboost_Pre-gen.ipynb ./ABC_GAN_Catboost_Output/ABC-GAN_output_${i}_${runs}.ipynb -p variance ${V} 
+        ((i=i+1))
+    done 
+done
