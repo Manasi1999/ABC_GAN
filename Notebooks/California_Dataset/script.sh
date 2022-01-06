b6f3aa1d058ea392bdd05d8ba968441445b41133
Manasi1999 <manasigude@gmail.com>
2022-01-06 16:13:02 +0530

    Results added

--- Notebooks/California_Dataset/script.sh
+++ Notebooks/California_Dataset/script.sh
@@ -1,10 +1,10 @@
 # Run Baseline Models  
 
-# for runs in 1 2 3 4 5 6 7 8 9 10
-# do 
-#     papermill BaselineModels.ipynb ./BaseLine_Model_Output/baselineModels_output_${runs}.ipynb -k papermill-tutorial
+for runs in 1 2 3 4 5 6 7 8 9 10
+do 
+    papermill BaselineModels.ipynb ./BaseLine_Model_Output/baselineModels_output_${runs}.ipynb -k papermill-tutorial
 
-# done 
+done 
 
 #Run GAN Model 
 # for runs in 1 2 3 4 5 6 7 8 9 10
@@ -42,15 +42,15 @@
 # done 
 
 #ABC-GAN with Catboost Pre-generator 
-for runs in 1 2 3 4 5 6 7 8 9 10
-do
-    i=0
-    for V in 1 0.1 0.01 
-    do 
-        papermill ABC_GAN_Model-Catboost_Pre-gen.ipynb ./ABC_GAN_Catboost_Output/ABC-GAN_output_${i}_${runs}.ipynb -p variance ${V} 
-        ((i=i+1))
-    done 
-done
+# for runs in 1 2 3 4 5 6 7 8 9 10
+# do
+#     i=0
+#     for V in 1 0.1 0.01 
+#     do 
+#         papermill ABC_GAN_Model-Catboost_Pre-gen.ipynb ./ABC_GAN_Catboost_Output/ABC-GAN_output_${i}_${runs}.ipynb -p variance ${V} 
+#         ((i=i+1))
+#     done 
+# done
 
 
 #Analysis 
