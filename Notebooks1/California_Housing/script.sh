92b3477831e87b79c72bcb2ab3e2f8c89e612cd0
Manasi1999 <manasigude@gmail.com>
2022-03-11 22:41:45 +0530

    Boston Output

--- Notebooks1/California_Housing/script.sh
+++ Notebooks1/California_Housing/script.sh
@@ -1,9 +1,9 @@
 #Run Baseline Models  
-for runs in 1 2 3 4 5 6 7 8 9 10
-do 
-    papermill BaselineModels.ipynb ./BaseLine_Model_Output/BaselineModels_output_${runs}.ipynb
+# for runs in 1 2 3 4 5 6 7 8 9 10
+# do 
+#     papermill BaselineModels.ipynb ./BaseLine_Model_Output/BaselineModels_output_${runs}.ipynb
 
-done 
+# done 
 
 #GAN 
 # for runs in 1 2 3 4 5 6 7 8 9 10
@@ -11,17 +11,17 @@ done
 #     papermill GAN.ipynb ./GAN_Output/GAN_output_${runs}_${i}.ipynb  
 # done
 
-
+papermill ABC_GAN-Stats.ipynb ./ABC_GAN_Stats/ABC-GAN_output_1_2.ipynb -p variance 0.01 
 # #ABC-GAN - Stats 
-# for runs in 1 2 3 4 5 6 7 8 9 10
-# do
-#     i=0
-#     for V in 1 0.1 0.01 
-#     do 
-#         papermill ABC_GAN-Stats.ipynb ./ABC_GAN_Stats/ABC-GAN_output_${runs}_${i}.ipynb -p variance ${V} 
-#         ((i=i+1))
-#     done 
-# done
+for runs in 2 3 4 5 6 7 8 9 10
+do
+    i=0
+    for V in 1 0.1 0.01 
+    do 
+        papermill ABC_GAN-Stats.ipynb ./ABC_GAN_Stats/ABC-GAN_output_${runs}_${i}.ipynb -p variance ${V} 
+        ((i=i+1))
+    done 
+done
 
 # #ABC-GAN - Catboost 
 # for runs in 1 2 3 4 5 6 7 8 9 10
