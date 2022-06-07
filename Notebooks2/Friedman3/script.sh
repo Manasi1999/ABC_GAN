a0900c1b82999a3c680b4936f243e78a9d045303
Manasi1999 <manasigude@gmail.com>
2022-06-07 17:17:58 +0530

    Experiment 1.1.2 Added

--- Notebooks2/Friedman3/script.sh
+++ Notebooks2/Friedman3/script.sh
@@ -1,9 +1,9 @@
 # Run Baseline Models  
-for runs in 1 2 3 4 5 6 7 8 9 10
-do 
-    papermill BaselineModels.ipynb ./BaseLine_Model_Output/BaselineModels_output_${runs}.ipynb
+# for runs in 1 2 3 4 5 6 7 8 9 10
+# do 
+#     papermill BaselineModels.ipynb ./BaseLine_Model_Output/BaselineModels_output_${runs}.ipynb
 
-done 
+# done 
 
 # #GAN 
 # # for runs in 1 2 3 4 5 6 7 8 9 10
@@ -13,21 +13,21 @@ done
 
 
 # #ABC-GAN - Stats 
-for runs in  10
-do
-    i=0
-    for B in 1 0.1 0.01 
-    do 
-        for V in 1 0.1 0.01 
-        do 
-            papermill ABC_GAN-Stats.ipynb ./ABC_GAN_Stats/ABC-GAN_output_${runs}_${i}.ipynb -p variance ${V} -p bias ${B}
-            ((i=i+1))
-        done 
-    done 
-done
+# for runs in 10
+# do
+#     i=0
+#     for B in 1 0.1 0.01 
+#     do 
+#         for V in 1 0.1 0.01 
+#         do 
+#             papermill ABC_GAN-Stats.ipynb ./ABC_GAN_Stats/ABC-GAN_output_${runs}_${i}.ipynb -p variance ${V} -p bias ${B}
+#             ((i=i+1))
+#         done 
+#     done 
+# done
 
 #ABC-GAN - Catboost 
-for runs in 1 2 3 4 5 6 7 8 9 10
+for runs in  10
 do
     i=0
     for B in 1 0.1 0.01 
