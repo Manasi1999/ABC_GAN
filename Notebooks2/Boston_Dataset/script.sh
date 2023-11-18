7f3d0bdfb0e6069650940d933f7e33c97f71576f
Manasi1999 <manasigude@gmail.com>
2023-11-18 23:02:50 +0530

    Update script file

--- Notebooks2/Boston_Dataset/script.sh
+++ Notebooks2/Boston_Dataset/script.sh
@@ -10,33 +10,33 @@
 # do
 #     papermill GAN.ipynb ./GAN_Output/GAN_output_${runs}_${i}.ipynb  
 # done
-papermill ABC_GAN-Stats.ipynb ./ABC_GAN_Stats/ABC-GAN_output_5_5.ipynb -p variance 0.01 -p bias 0.2
-for runs in 5
-do
-    i=6
-    for B in 0.01 
-    do 
-        for V in 1 0.1 0.01 
-        do 
-            papermill ABC_GAN-Stats.ipynb ./ABC_GAN_Stats/ABC-GAN_output_${runs}_${i}.ipynb -p variance ${V} -p bias ${B}
-            ((i=i+1))
-        done 
-    done 
-done
+# papermill ABC_GAN-Stats.ipynb ./ABC_GAN_Stats/ABC-GAN_output_5_5.ipynb -p variance 0.01 -p bias 0.2
+# for runs in 5
+# do
+#     i=6
+#     for B in 0.01 
+#     do 
+#         for V in 1 0.1 0.01 
+#         do 
+#             papermill ABC_GAN-Stats.ipynb ./ABC_GAN_Stats/ABC-GAN_output_${runs}_${i}.ipynb -p variance ${V} -p bias ${B}
+#             ((i=i+1))
+#         done 
+#     done 
+# done
 
 #ABC-GAN - Stats 
-for runs in 6 7 8 9 10
-do
-    i=0
-    for B in 1 0.1 0.01 
-    do 
-        for V in 1 0.1 0.01 
-        do 
-            papermill ABC_GAN-Stats.ipynb ./ABC_GAN_Stats/ABC-GAN_output_${runs}_${i}.ipynb -p variance ${V} -p bias ${B}
-            ((i=i+1))
-        done 
-    done 
-done
+# for runs in 6 7 8 9 10
+# do
+#     i=0
+#     for B in 1 0.1 0.01 
+#     do 
+#         for V in 1 0.1 0.01 
+#         do 
+#             papermill ABC_GAN-Stats.ipynb ./ABC_GAN_Stats/ABC-GAN_output_${runs}_${i}.ipynb -p variance ${V} -p bias ${B}
+#             ((i=i+1))
+#         done 
+#     done 
+# done
 
 #ABC-GAN - Catboost 
 for runs in 1 2 3 4 5 6 7 8 9 10
